919747fce8e238d94fd86fc9e4f04211174ad4a3
Nimah Alyamani <66944123+TGOT1001Nimah@users.noreply.github.com>
2020-12-29 16:04:03 +0300

    task 3 with a smart method in 7 meeting
    here to see how the Relay working in the circuit, after that we will change the light to Solenoid Air Valve

---
+++ trying_relay.ino
@@ -0,0 +1,16 @@
+int relay_pin = 8;
+
+
+void setup(){ 
+  pinMode(relay_pin,OUTPUT);
+}
+
+
+
+void loop(){
+  digitalWrite(relay_pin,HIGH);
+  delay(5000);
+  digitalWrite(relay_pin,LOW);
+  delay(5000);
+}
+//this is to see how the Relay working in the circuit, after that we will change the light to Solenoid Air Valve 
